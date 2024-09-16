e15b4911264fa3bbc0ae9355b148cc9d7fc4d0ac
Navin Subramanian <assasin71003@gmail.com>
2024-09-16 13:31:28 +0530

    connection string v2

--- api/config/db.js
+++ api/config/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
     try {
         const uri = process.env.MONGODB_URI;
-        if (!uri) {
-            throw new Error('MongoDB URI is not defined');
-        }
+        // if (!uri) {
+        //     throw new Error('MongoDB URI is not defined');
+        // }
         const conn = await mongoose.connect("mongodb://nnavinsubramaniancse2021:40OwVo3jyL117KIK@testcluster-shard-00-00.ovyt5.mongodb.net:27017,testcluster-shard-00-01.ovyt5.mongodb.net:27017,testcluster-shard-00-02.ovyt5.mongodb.net:27017/?ssl=true&",{
             retryWrites: true,
             w: 'majority',
